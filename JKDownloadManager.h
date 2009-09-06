5e3ad22301c071267c7659295d8a0b269f83b446
Joost Koehoorn <joost.koehoorn@gmail.com>
2009-09-06 10:41:53 +0200

    first commit

---
+++ JKDownloadManager.h
@@ -0,0 +1,249 @@
+/**
+ * JKDownloadManager v1.1
+ * ---------------------
+ * 6 September 2009
+ * 
+ * Release notes:
+ *		v1.1 (6 September 2009)
+ *			- Added status code property
+ *			- New context pointer as a replacement of the downloadId
+ *			- Open sourced under the MIT license
+ *		v1.0 (26 August 2009)
+ *			- Initial launch
+ * 
+ * Known issues:
+ *		- none
+ * 
+ * License:
+ * 		Copyright (c) 2009 Joost Koehoorn
+ * 		
+ * 		Permission is hereby granted, free of charge, to any person
+ * 		obtaining a copy of this software and associated documentation
+ * 		files (the "Software"), to deal in the Software without
+ * 		restriction, including without limitation the rights to use,
+ * 		copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * 		copies of the Software, and to permit persons to whom the
+ * 		Software is furnished to do so, subject to the following
+ * 		conditions:
+ * 		
+ * 		The above copyright notice and this permission notice shall be
+ * 		included in all copies or substantial portions of the Software.
+ * 		
+ * 		THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * 		EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+ * 		OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * 		NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ * 		HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ * 		WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * 		FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * 		OTHER DEALINGS IN THE SOFTWARE.
+ * 
+ */
+#import <Foundation/Foundation.h>
+
+
+@class JKDownloadManager, JKDownload;
+
+
+@protocol JKDownloadManagerDelegate <NSObject>
+
+@optional
+
+/**
+ * Called when a download object did finish loading succesfully
+ * 
+ * @param	id		Download object which did finish loading
+ */
+- (void)downloadDidFinishLoading:(JKDownload *)download;
+
+/**
+ * Called when a download object failed to load
+ * 
+ * @param	id		Download object which did fail loading
+ * @param	id		Error object. Created by the connection object
+ */
+- (void)download:(JKDownload *)download didFailWithError:(NSError *)error;
+
+/**
+ * Called when all downloads in a stack have finished loading.
+ * This method will always be sent, even when some downloads did
+ * fail loading
+ * 
+ * @param	id		Shared download manager object
+ * @param	id		Array with all the downloads objects
+ */
+- (void)downloadManager:(JKDownloadManager *)downloadManager didFinishLoadingDownloadsInStack:(NSArray *)downloads;
+
+@end
+
+
+/**
+ * The download class interface.
+ *
+ * @protocol NSCopying	Only the request object is copied. The context pointer stays untouched
+ */
+@interface JKDownload : NSObject <NSCopying> {
+	NSURLRequest *_request;
+	NSURLConnection *_connection;
+	NSMutableData *_data;
+	NSError *_error;
+	NSInteger _statusCode;
+	id _context;
+	
+	NSString *_stackId;
+	id <JKDownloadManagerDelegate> _delegate; // Weak reference
+	BOOL _finished;
+}
+/**
+ * The URL request object which is being performed
+ */
+@property(nonatomic, readonly) NSURLRequest *request;
+
+/**
+ * The connection object performs the request.
+ * Do not mess with it's delegate! Pointer will
+ * be zeroud out when downloading did finish
+ */
+@property(nonatomic, readonly) NSURLConnection *connection;
+
+/**
+ * The data object which contains the response data
+ * This should only be read when the download did finish loading
+ */
+@property(nonatomic, readonly) NSMutableData *data;
+
+/**
+ * An error object which will only been set when an error occured
+ */
+@property(nonatomic, readonly) NSError *error;
+
+/**
+ * The status code of the request. Default of -1
+ */
+@property(nonatomic, readonly) NSInteger statusCode;
+
+/**
+ * A context object which you can use to identify the download
+ * or just as a reference pointer to some object
+ */
+@property(nonatomic, retain) id context;
+
+/**
+ * The id of the stack this download is performed in.
+ * Will be nil when download is individual
+ */
+@property(nonatomic, readonly) NSString *stackId;
+
+/**
+ * The download's delegate. See the JKDownloadManagerDelegate
+ * protocol for all the protocol methods. Weak reference!
+ */
+@property(nonatomic, readonly) id <JKDownloadManagerDelegate> delegate;
+
+/**
+ * A boolean which indicates whether we have finished loading.
+ * Will be set to YES even when you cancel the connection using -cancel
+ */
+@property(nonatomic, readonly, getter=isFinished) BOOL finished;
+
+/**
+ * Plain string initializers
+ *
+ * @param	id		The url string to be downloaded
+ * @param	id		Context object to be used for your own reference
+ * @return	id		Newly initialized download object
+ */
+- (JKDownload *)initWithURLString:(NSString *)URLString;
+- (JKDownload *)initWithURLString:(NSString *)URLString context:(id)context;
+
+/**
+ * Plain URL initializers
+ *
+ * @param	id		The url object to be downloaded
+ * @param	id		Context object to be used for your own reference
+ * @return	id		Newly initialized download object
+ */
+- (JKDownload *)initWithURL:(NSURL *)URL;
+- (JKDownload *)initWithURL:(NSURL *)URL context:(id)context;
+
+/**
+ * Advanced initializers
+ *
+ * @param	id		The url request object to be used
+ * @param	id		Context object to be used for your own reference
+ * @return	id		Newly initialized download object
+ */
+- (JKDownload *)initWithURLRequest:(NSURLRequest *)request;
+- (JKDownload *)initWithURLRequest:(NSURLRequest *)request context:(id)context;
+
+/**
+ * Autoreleased class methods
+ *
+ * @return	id		An autoreleased download object
+ */
++ (JKDownload *)downloadWithURLString:(NSString *)URLString;
++ (JKDownload *)downloadWithURLString:(NSString *)URLString context:(id)context;
++ (JKDownload *)downloadWithURL:(NSURL *)URL;
++ (JKDownload *)downloadWithURL:(NSURL *)URL context:(id)context;
++ (JKDownload *)downloadWithURLRequest:(NSURLRequest *)request;
++ (JKDownload *)downloadWithURLRequest:(NSURLRequest *)request context:(id)context;
+
+/**
+ * Tell the download object to begin downloading.
+ *
+ * @param	id		Delegate object, must conform to the JKDownloadManagerDelegate protocol
+ * @return	void
+ */
+- (void)performWithDelegate:(id <JKDownloadManagerDelegate>)delegate;
+
+/**
+ * Cancel downloading process. This will set the finished flag to YES.
+ * You cannot use the download object again, you have to copy it
+ *
+ * @return	void
+ */
+- (void)cancel;
+
+@end
+
+
+@interface JKDownloadManager : NSObject {
+	NSMutableDictionary *_stack;
+}
+
+/**
+ * The shared download manager handles multiple downloads at once
+ *
+ * @return	id		Shared download manager
+ */
++ (JKDownloadManager *)sharedManager;
+
+/**
+ * Same as calling -[JKDownload performWithDelegate:]
+ *
+ * @param	id		The download object to be performed
+ * @param	id		The delegate of the download object
+ * @return	void
+ */
+- (void)performDownload:(JKDownload *)download withDelegate:(id <JKDownloadManagerDelegate>)delegate;
+
+/**
+ * Perform multiple downloads at once
+ *
+ * @param	id		Array of download objects to be performed at one time
+ * @param	id		Delegate of the download objects
+ * @param	id		The id of the stack. Can be used to cancel all downloads in a stack at once
+ * @return	void
+ */
+- (void)performDownloads:(NSArray *)downloads withDelegate:(id <JKDownloadManagerDelegate>)delegate stackId:(NSString *)stackId;
+
+/**
+ * Cancel all the downloads in a stack at once. This will NOT call the delegate
+ * that it has finished downloading all the downloads in the stack.
+ *
+ * @param	id		The id of the stack of which all downloads should be cancelled
+ * @return	void
+ */
+- (void)cancelDownloadsInStackWithId:(NSString *)stackId;
+
+@end
---
+++ JKDownloadManager.m
@@ -0,0 +1,390 @@
+/**
+ * JKDownloadManager v1.1
+ * ---------------------
+ * 6 September 2009
+ * 
+ * License:
+ * 		Copyright (c) 2009 Joost Koehoorn
+ * 		
+ * 		Permission is hereby granted, free of charge, to any person
+ * 		obtaining a copy of this software and associated documentation
+ * 		files (the "Software"), to deal in the Software without
+ * 		restriction, including without limitation the rights to use,
+ * 		copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * 		copies of the Software, and to permit persons to whom the
+ * 		Software is furnished to do so, subject to the following
+ * 		conditions:
+ * 		
+ * 		The above copyright notice and this permission notice shall be
+ * 		included in all copies or substantial portions of the Software.
+ * 		
+ * 		THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * 		EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+ * 		OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * 		NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ * 		HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ * 		WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * 		FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * 		OTHER DEALINGS IN THE SOFTWARE.
+ * 
+ */
+#import "JKDownloadManager.h"
+#import "AppController.h"
+
+
+@interface JKDownload (Internal)
+
+- (void)_setStackId:(NSString *)stackId;
+- (void)_openConnection;
+- (void)_resetConnection;
+
+@end
+
+
+
+@interface JKDownloadManager (Internal)
+
+- (void)_downloadDidFinish:(JKDownload *)download;
+
+@end
+
+
+
+
+
+@implementation JKDownload
+
+@synthesize request = _request, context = _context, connection = _connection, data = _data, error = _error,
+			statusCode = _statusCode, stackId = _stackId, delegate = _delegate, finished = _finished;
+
+#pragma mark -
+#pragma mark Initialization
+
+- (id)init {
+	[self release];
+	return nil;
+}
+
+- (JKDownload *)initWithURLString:(NSString *)URLString {
+	return [self initWithURLString:URLString context:nil];
+}
+
+- (JKDownload *)initWithURLString:(NSString *)URLString context:(id)context {
+	return [self initWithURL:[NSURL URLWithString:URLString] context:context];
+}
+
+- (JKDownload *)initWithURL:(NSURL *)url {
+	return [self initWithURL:url context:nil];
+}
+
+- (JKDownload *)initWithURL:(NSURL *)url context:(id)context {
+	return [self initWithURLRequest:[NSURLRequest requestWithURL:url] context:context];
+}
+
+- (JKDownload *)initWithURLRequest:(NSURLRequest *)request {
+	return [self initWithURLRequest:request context:nil];
+}
+
+- (JKDownload *)initWithURLRequest:(NSURLRequest *)request context:(id)context {
+	if(self = [super init]){
+		_request = [request retain];
+		_context = [context retain];
+		_statusCode = -1;
+	}
+	
+	return self;
+}
+
++ (JKDownload *)downloadWithURLString:(NSString *)URLString {
+	return [[[JKDownload alloc] initWithURLString:URLString] autorelease];
+}
+
++ (JKDownload *)downloadWithURLString:(NSString *)URLString context:(id)context {
+	return [[[JKDownload alloc] initWithURLString:URLString context:context] autorelease];
+}
+
++ (JKDownload *)downloadWithURL:(NSURL *)url {
+	return [[[JKDownload alloc] initWithURL:url] autorelease];
+}
+
++ (JKDownload *)downloadWithURL:(NSURL *)url context:(id)context {
+	return [[[JKDownload alloc] initWithURL:url context:context] autorelease];
+}
+
++ (JKDownload *)downloadWithURLRequest:(NSURLRequest *)request {
+	return [[[JKDownload alloc] initWithURLRequest:request] autorelease];
+}
+
++ (JKDownload *)downloadWithURLRequest:(NSURLRequest *)request context:(id)context {
+	return [[[JKDownload alloc] initWithURLRequest:request context:context] autorelease];
+}
+
+#pragma mark -
+#pragma mark NSCopying protocol methods
+
+- (id)copyWithZone:(NSZone *)zone {
+	JKDownload *copy = [[[self class] allocWithZone:zone] initWithURLRequest:[_request copy] context:context];
+	
+	return copy;
+}
+
+#pragma mark -
+#pragma mark Implementation
+
+- (void)performWithDelegate:(id <JKDownloadManagerDelegate>)delegate {
+	_delegate = delegate;
+	[self _openConnection];
+}
+
+- (void)cancel {
+	[self _resetConnection];
+}
+
+#pragma mark -
+#pragma mark NSURLConnectionDelegate methods
+
+- (void)connection:(NSURLConnection *)connection didReceiveResponse:(NSURLResponse *)response {
+	// Set status code when available
+	if([response isKindOfClass:[NSHTTPURLResponse class]]){
+		_statusCode = [(NSHTTPURLResponse *)response statusCode];
+	}
+	
+	[_data setLength:0];
+}
+
+- (NSURLRequest *)connection:(NSURLConnection *)connection willSendRequest:(NSURLRequest *)request redirectResponse:(NSURLResponse *)redirectResponse {
+	return request;
+}
+
+- (void)connection:(NSURLConnection *)connection didReceiveData:(NSData *)received {
+	[_data appendData:received];
+}
+
+- (void)connection:(NSURLConnection *)connection didFailWithError:(NSError *)error {
+	// Done with this connection
+	[self _resetConnection];
+	
+	// Store error
+	[_error release];
+	_error = [error retain];
+	
+	// Call delegate
+	if([_delegate respondsToSelector:@selector(download:didFailWithError:)]){
+		[_delegate download:self didFailWithError:_error];
+	}
+}
+
+- (void)connectionDidFinishLoading:(NSURLConnection *)connection {
+	// Done with this connection
+	[self _resetConnection];
+	
+	// Call delegate
+	if([_delegate respondsToSelector:@selector(downloadDidFinishLoading:)]){
+		[_delegate downloadDidFinishLoading:self];
+	}
+}
+
+#pragma mark -
+
+- (void)dealloc {
+	[self cancel];
+	
+	[_request release], _request = nil;
+	[_connection release], _connection = nil;
+	[_data release], _data = nil;
+	[_error release], _error = nil;
+	[_context release], _context = nil;
+	[_stackId release], _stackId = nil;
+	
+	[super dealloc];
+}
+
+@end
+
+
+#pragma mark -
+#pragma mark Internal
+
+
+@implementation JKDownload (Internal)
+
+- (void)_setStackId:(NSString *)stackId {
+	if(stackId != _stackId){
+		[_stackId release];
+		_stackId = [stackId retain];
+	}
+}
+
+- (void)_openConnection {
+	// Perform only when not finished yet
+	if(_finished || _connection != nil) return;
+	
+	// Create data object
+	if(_data == nil){
+		_data = [[NSMutableData alloc] init];
+	}
+	
+	// Create connection
+	_connection = [[NSURLConnection alloc] initWithRequest:_request delegate:self];
+	[[AppController sharedController] incrementNumberOfConnections];
+}
+
+- (void)_resetConnection {
+	if(_connection != nil){
+		// Mark as finished
+		_finished = YES;
+		[[JKDownloadManager sharedManager] _downloadDidFinish:self];
+		
+		// Release connection
+		[_connection cancel];
+		[_connection release], _connection = nil;
+		[[AppController sharedController] decrementNumberOfConnections];
+	}
+}
+
+@end
+
+
+#pragma mark -
+#pragma mark JKDownloadManager
+#pragma mark -
+
+@implementation JKDownloadManager
+
+static JKDownloadManager *sharedDownloadManagerInstance = nil;
+
+#pragma mark -
+#pragma mark Singleton implementation
+
++ (JKDownloadManager *)sharedManager {
+    @synchronized(self){
+        if(sharedDownloadManagerInstance == nil){
+            [[self alloc] init];
+        }
+    }
+    return sharedDownloadManagerInstance;
+}
+
++ (id)allocWithZone:(NSZone *)zone {
+    @synchronized(self){
+        if(sharedDownloadManagerInstance == nil){
+            sharedDownloadManagerInstance = [super allocWithZone:zone];
+            return sharedDownloadManagerInstance;
+        }
+    }
+    return nil;
+}
+
+- (id)copyWithZone:(NSZone *)zone {
+    return self;
+}
+
+- (id)retain {
+    return self;
+}
+
+- (NSUInteger)retainCount {
+    return NSUIntegerMax;
+}
+
+- (void)release {
+    // Ignore, singleton instance
+}
+
+- (id)autorelease {
+    return self;
+}
+
+#pragma mark -
+#pragma mark Implementation
+
+- (void)performDownload:(JKDownload *)download withDelegate:(id <JKDownloadManagerDelegate>)delegate {
+	[download performWithDelegate:delegate];
+}
+
+- (void)performDownloads:(NSArray *)downloads withDelegate:(id <JKDownloadManagerDelegate>)delegate stackId:(NSString *)stackId {
+	// Stack id must not be nil
+	if(stackId == nil){
+		[NSException raise:NSInvalidArgumentException format:@"%s: stackId must not be nil.", __PRETTY_FUNCTION__];
+	}
+	
+	// Create stack object
+	if(_stack == nil){
+		_stack = [[NSMutableDictionary alloc] init];
+	}
+	
+	// Add objects to stack
+	[_stack setObject:downloads forKey:stackId];
+	
+	// Cycle through downloads
+	for(JKDownload *download in downloads){
+		[download _setStackId:stackId];
+		[download performWithDelegate:delegate];
+	}
+}
+
+- (void)cancelDownloadsInStackWithId:(NSString *)stackId {
+	// Get stack array
+	NSArray *stack = [_stack objectForKey:stackId];
+	
+	// Remove stack from stacks array, so that we will
+	// ignore the fact that the download did finish
+	[[stack retain] autorelease];
+	[_stack removeObjectForKey:stackId];
+	
+	// Cancel all downloads
+	for(JKDownload *download in stack){
+		[download cancel];
+	}
+}
+
+#pragma mark -
+
+- (void)dealloc {
+	[_stack release], _stack = nil;
+	
+	[super dealloc];
+}
+
+@end
+
+#pragma mark -
+#pragma mark Internal
+
+@implementation JKDownloadManager (Internal)
+
+- (void)_downloadDidFinish:(JKDownload *)download {
+	// When the download was not performed within a stack,
+	// Ignore the fact that it did finish. The download
+	// will call it's delegate itself.
+	if(download.stackId == nil) return;
+	
+	// Get stack array. We have to check whether it is nil,
+	// because when all downloads in a stack are being cancelled,
+	// we don't want to call our delegate
+	NSArray *stack = [_stack objectForKey:download.stackId];
+	if(stack != nil){
+		// See whether we have finished all the dowloads in the stack
+		BOOL finished = YES;
+		for(JKDownload *_download in stack){
+			if(_download.finished == NO){
+				finished = NO;
+				break;
+			}
+		}
+		
+		// When finished all downloads
+		if(finished){
+			// Remove from stack
+			[[stack retain] autorelease];
+			[_stack removeObjectForKey:download.stackId];
+			
+			// Call delegate
+			if([download.delegate respondsToSelector:@selector(downloadManager:didFinishLoadingDownloadsInStack:)]){
+				[download.delegate downloadManager:self didFinishLoadingDownloadsInStack:stack];
+			}
+		}
+	}
+}
+
+@end
