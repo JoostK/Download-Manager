e3ec2a9a0a586caa891f5023404b747d3cd6400d
Joost Koehoorn <joost.koehoorn@gmail.com>
2009-09-09 19:06:56 +0200

    Edited comments

--- main.m
+++ main.m
@@ -1,11 +1,6 @@
-//
-//  main.m
-//  Download-Manager
-//
-//  Created by Joost on 08-09-09.
-//  Copyright 2009 Qdevelopment. All rights reserved.
-//
-
+// 
+// main.m
+// 
 #import <Cocoa/Cocoa.h>
 
 int main(int argc, char *argv[])
