4509140839fcd10d9bc0ac32a9e201e4f235400e
Joost Koehoorn <joost.koehoorn@gmail.com>
2009-09-09 18:56:29 +0200

    Added Xcode project and Sample application

---
+++ main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  Download-Manager
+//
+//  Created by Joost on 08-09-09.
+//  Copyright 2009 Qdevelopment. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc,  (const char **) argv);
+}
